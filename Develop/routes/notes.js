c71b3430915250a1abe227d042cf1f2e9af4cc18
Kevin Hogan <kchogan@pacbell.net>
2024-01-27 14:59:44 -0800

    basic structure and first get and post notes implemented

---
+++ Develop/routes/notes.js
@@ -0,0 +1,30 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { readFromFile, readAndAppend, writeToFile, } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      note_id: uuidv4(),
+      title,
+      text,
+    };
+
+    readAndAppend(newNote, './db/notes.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
