c0eee54cdc229ef6eb2d06c90d90e9ef1ca4211f
Kevin Hogan <kchogan@pacbell.net>
2024-01-27 15:57:50 -0800

    implemented deletion of notes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,12 +2,29 @@ const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend, writeToFile, } = require('../helpers/fsUtils');
 
-// GET Route for retrieving all the tips
+// GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
   readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
+// DELETE Route for a specific note
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/notes.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all notes except the one with the ID provided in the URL
+        const result = json.filter((note) => note.id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/notes.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted`);
+      });
+  });
+
+// POST Route for a new UX/UI note
 notes.post('/', (req, res) => {
   console.log(req.body);
 
