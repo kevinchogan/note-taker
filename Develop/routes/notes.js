85d7ca09d60f462ee53c3f3b9ed5af77fc1ae57e
Kevin Hogan <kchogan@pacbell.net>
2024-01-27 16:01:02 -0800

    code formatting

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,31 +1,35 @@
-const notes = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const { readFromFile, readAndAppend, writeToFile, } = require('../helpers/fsUtils');
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
-  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+notes.get("/", (req, res) => {
+  readFromFile("./db/notes.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // DELETE Route for a specific note
-notes.delete('/:id', (req, res) => {
-    const noteId = req.params.id;
-    readFromFile('./db/notes.json')
-      .then((data) => JSON.parse(data))
-      .then((json) => {
-        // Make a new array of all notes except the one with the ID provided in the URL
-        const result = json.filter((note) => note.id !== noteId);
-  
-        // Save that array to the filesystem
-        writeToFile('./db/notes.json', result);
-  
-        // Respond to the DELETE request
-        res.json(`Item ${noteId} has been deleted`);
-      });
-  });
+notes.delete("/:id", (req, res) => {
+  const noteId = req.params.id;
+  readFromFile("./db/notes.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile("./db/notes.json", result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted`);
+    });
+});
 
 // POST Route for a new UX/UI note
-notes.post('/', (req, res) => {
+notes.post("/", (req, res) => {
   console.log(req.body);
 
   const { title, text } = req.body;
@@ -37,10 +41,10 @@ notes.post('/', (req, res) => {
       text,
     };
 
-    readAndAppend(newNote, './db/notes.json');
+    readAndAppend(newNote, "./db/notes.json");
     res.json(`Note added successfully 🚀`);
   } else {
-    res.error('Error in adding note');
+    res.error("Error in adding note");
   }
 });
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const express = require("express");
+const path = require("path");
+const { clog } = require("./middleware/clog");
+const api = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,23 +13,23 @@ app.use(clog);
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use("/api", api);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 // Wildcard route to direct users to a 404 page
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/404.html'))
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/404.html"))
 );
 
 app.listen(PORT, () =>
